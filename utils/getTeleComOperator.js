fb73fbb28f3338a553044f3e5db701afce842315
Tin Maung Linn <tml@za.com.mm>
2024-10-26 15:08:08 +0630

    Update Thailand Telecommunication Operators

--- utils/getTeleComOperator.js
+++ utils/getTeleComOperator.js
@@ -2,7 +2,20 @@
 
 import { format_phone } from './phoneFormatter';
 
-export const getTelecomOperatorName = (phoneNumber) => {
+export const getTelecomOperatorName = (phoneNumber,countryCode) => {
+    switch(countryCode) {
+        case '95':
+            return getMyanmarTeleComOperators(phoneNumber);
+        case '66':
+            return getThailandTeleComOperators(phoneNumber);
+        case '65':
+            return getSingaporeTeleComOperators(phoneNumber);
+        default:
+            return 'Unsupported country code';
+    }
+};
+
+export function getMyanmarTeleComOperators(phoneNumber) {
     if (!phoneNumber) {
         return 'Phone number is required';
       }
@@ -25,9 +38,95 @@ export const getTelecomOperatorName = (phoneNumber) => {
     if (telecomOperators[prefix].name === "MPT" || telecomOperators[prefix].name === "MEC") {
       return phoneNumber.length < 8 || phoneNumber.length > 11 ? 'Invalid phone number' : telecomOperators[prefix] || 'Unknown number';
     }else{
-      return phoneNumber.length < 11 || phoneNumber.length > 11 ? 'Invalid phone number' : telecomOperators[prefix] || 'Unknown number';
+      return phoneNumber.length !== 11 ? 'Invalid phone number' : telecomOperators[prefix] || 'Unknown number';
     }
-};
+}
+
+export function getThailandTeleComOperators(phoneNumber) {
+    if (!phoneNumber) {
+        return 'Phone number is required';
+      }
+
+    const telecomOperators = {};
+
+    const dtacPrefixes = ["085","089", "084"];
+
+    dtacPrefixes.forEach(prefix => {
+        telecomOperators[prefix] = { name: "DTAC", logo: "https://access.cdndata.cloud/marketplace/svg/mpt.svg" };
+    });
+
+    const trueMoveHPrefixes = ["083","084", "088"];
+    trueMoveHPrefixes.forEach(prefix => {
+        telecomOperators[prefix] = {name: "TrueMoveH", logo: "https://access.cdndata.cloud/marketplace/svg/mpt.svg"};
+    })
+
+    const aisPrefixes = ["080","081","090"];
+    aisPrefixes.forEach(prefix=> {
+        telecomOperators[prefix] = {name: "AIS", logo: "https://access.cdndata.cloud/marketplace/svg/mpt.svg"};
+    })
+    
+    const formattedPhoneNumber = format_phone(phoneNumber);
+    const prefix = formattedPhoneNumber.substring(0, 3);
+    
+    return phoneNumber.length !== 7 ? 'Invalid phone number' : telecomOperators[prefix] || 'Unknown number';
+    
+}
+
+export function getSingaporeTeleComOperators(phoneNumber) {
+    return 'Singapore Telecom Operators';
+}
+
+
+export function getCountryCodes() {
+    return [
+            {
+                country : 'Myanmar',
+                code : '95',
+                flag : 'mm-flag.svg',
+                
+            },
+            {
+                country : 'Singapore',
+                code : '65',
+                flag : 'singapore-flag.svg',
+            },
+            {
+                country : 'Thailand',
+                code : '66',
+                flag : 'thai-flag.svg',
+            },
+            // {
+            //     country : 'Malaysia',
+            //     code : '60',
+            //     flag : 'malaysia-flag.svg',
+            // },
+            // {
+            //     country : 'China',
+            //     code : '86',
+            //     flag : 'china-flag.svg',
+            // },
+            // {
+            //     country : 'USA',
+            //     code : '1',
+            //     flag : 'US-flag.svg',
+            // },
+            // {
+            //     country : 'Japan',
+            //     code : '81',
+            //     flag : 'japan-flag.svg',
+            // },
+            // {
+            //     country : 'South Korea',
+            //     code : '82',
+            //     flag : 'korea-flag.svg',
+            // },
+            // {
+            //     country : 'India',
+            //     code : '91',
+            //     flag : 'india-flag.svg',
+            // },
+        ]
+}
 
 module.exports = {
     getTelecomOperatorName,
