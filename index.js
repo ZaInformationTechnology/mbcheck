af657d33f88aacd05d1deaaca4ae9fd408dabedf
Tin Maung Linn <tml@za.com.mm>
2024-10-26 13:16:32 +0630

    Update import err

--- index.js
+++ index.js
@@ -2,4 +2,3 @@
 
 export { getTelecomOperatorName } from './composables/useTelecomOperator';
 export { useCountryCodes } from './composables/countryCodes';
-export { default as PhoneNumberInput } from './components/PhoneNumberInput.vue';
