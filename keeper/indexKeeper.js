6c6c0928d6f3572924f02a48f0279012d2eded6f
Tin Maung Linn <tml@za.com.mm>
2024-11-07 15:43:45 +0630

    Update

--- keeper/indexKeeper.js
+++
@@ -1,4 +0,0 @@
-export { getTelecomOperatorName } from '../composables/useTelecomOperator';
-export { useCountryCodes } from '../composables/countryCodes';
-export { default as PhoneNumberInput } from './src/PhoneNumberInput.vue';
-export { default as CountryCodeSelectInput } from './components/CountryCodeSelectInput.vue';
